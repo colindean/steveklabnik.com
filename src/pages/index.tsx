5669445ff0041030a26822450845834c3546aedb
Steve Klabnik <steve@steveklabnik.com>
2020-06-09 08:41:52 -0500

    just redirect things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,35 +4,8 @@ import Header from '../components/header'
 export default () => (
   <>
     <header className="container header">
-      <h1 className="title">Hi there!</h1>
-      <h2 className="subtitle">I'm Steve.</h2>
+      <h1 className="title">Redirect time!</h1>
     </header>
-    <section className="container">
-      <h2>Steve Klabnik, that is.</h2>
-      <p>
-        You may know me from my work on <a href="https://rust-lang.org">Rust</a>
-        , or maybe even the stuff I did with{' '}
-        <a href="https://contributors.rubyonrails.org/contributors/steve-klabnik/commits">
-          Ruby on Rails
-        </a>{' '}
-        back in the day.
-      </p>
-      <p>
-        You might have read <Link href="/writing">a post I wrote</Link>, or
-        maybe even a chapter or two of{' '}
-        <em>
-          <a href="https://nostarch.com/Rust">The Rust Programming Language</a>
-        </em>
-        .
-      </p>
-      <p>
-        Maybe you saw a conference talk of mine, or saw one of my (too many,
-        frankly) <a href="https://twitter.com/steveklabnik">tweets</a>.
-      </p>
-      <p>
-        It's also possible that you've used some code I've developed{' '}
-        <a href="https://github.com/steveklabnik">on GitHub</a>.
-      </p>
-    </section>
+    <section className="container"></section>
   </>
 )
